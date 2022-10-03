6fa23947eeeecba44ccf47c4c5de841107331202
Jesse Nicolas <jn@nevada.unr.edu>
2022-10-03 09:18:05 -0700

    build models

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,27 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const arrangementSchema = new Schema({
+  name: String,
+  details: String,
+  fulfillment: Date,
+}, {
+  timestamps: true
+})
+
+const taskSchema = new Schema({
+  name: String,
+  details: String,
+  fulfilled: Boolean,
+  arrangements: [arrangementSchema]
+}, {
+  timestamps: true
+})
+
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  tasks: [taskSchema],
 }, {
   timestamps: true
 })
