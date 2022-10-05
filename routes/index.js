ad19f880c726c70c2b8e28590dac1627de34270f
Jesse Nicolas <jn@nevada.unr.edu>
2022-10-05 11:30:55 -0700

    authentication/landing page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import * as profilesCtrl from '../controllers/profiles.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 router.get('/', function (req, res) {
-  res.redirect(`/profiles/${req.user.profile._id}`)
+  if (req.user) {
+    res.redirect(`/profiles/${req.user.profile._id}`)
+  }
+  else {res.render('index', {
+    title: 'landing'
+  })}
 })
 
 export {
